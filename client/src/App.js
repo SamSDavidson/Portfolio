6dc94bd7744a5123b2856aba22d6acd2332209b7
Sam Davidson <39926619+SamSDavidson@users.noreply.github.com>
2020-06-17 18:18:40 -0400

    added landing and about pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import Landing from './pages/landing/index';
+import About from './pages/about/index';
+import Projects from './pages/projects/index';
+import Blog from './pages/blog/index';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route path="/" exact component={Landing} />
+        <Route path="/about" exact component={About} />
+        <Route path="/projects" exact component={Projects} />
+        <Route path="/blog" exact component={Blog} />
+      </Router>
     </div>
   );
 }
---
+++ client/src/pages/blog/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from '../../components/header/index';
+
+class Blog extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+  }
+}
+
+export default Blog;
