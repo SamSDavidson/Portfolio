1a021c5ba13d33ab06f79494b7b0d438e4e296dd
Sam Davidson <39926619+SamSDavidson@users.noreply.github.com>
2020-06-23 22:07:59 -0400

    updated build

---
+++ api/build/build/precache-manifest.7790fc43f01a8a0b6a2b38a232a2b263.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fd3fc932ff518c59ec8aea43b1ff4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfd50fbb7720f550485b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b8cda9c481b041d9a33f",
+    "url": "/static/css/main.47c8b084.chunk.css"
+  },
+  {
+    "revision": "bfd50fbb7720f550485b",
+    "url": "/static/js/2.1d5ef3f0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1d5ef3f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8cda9c481b041d9a33f",
+    "url": "/static/js/main.b047a4b2.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f3d1b5941907f67482227e82d9d798bf",
+    "url": "/static/media/paintSwatches.f3d1b594.jpg"
+  }
+]);
